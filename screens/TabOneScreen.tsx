c207254a704f5d32bce56a6083a6fc6d6e3c91df
Fabiano Ramos <fabiano.suporteinfo@gmail.com>
2020-06-26 18:43:11 -0300

    Webview test 2

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -15,16 +15,6 @@ export default function TabOneScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
   },
 });
+
