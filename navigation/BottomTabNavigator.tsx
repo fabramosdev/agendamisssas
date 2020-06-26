adf925765973b7efd0b0e80c4e0faeb5a2124b81
Fabiano Ramos <fabiano.suporteinfo@gmail.com>
2020-06-26 18:10:13 -0300

    Commit Inicial

--- navigation/BottomTabNavigator.tsx
+++ navigation/BottomTabNavigator.tsx
@@ -52,7 +52,7 @@ function TabOneNavigator() {
       <TabOneStack.Screen
         name="TabOneScreen"
         component={TabOneScreen}
-        options={{ headerTitle: 'Tab One Title' }}
+        options={{ headerTitle: 'ArqRio - Agenda Missas Versão Web' }}
       />
     </TabOneStack.Navigator>
   );
